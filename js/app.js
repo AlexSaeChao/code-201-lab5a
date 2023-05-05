8f6e2b9a1e547121587e782e5425f29f013276a8
Alex Chao <chaoalex93@gmail.com>
2023-05-05 21:33:24 +0000

    problem three passed test

--- js/app.js
+++ js/app.js
@@ -30,8 +30,8 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function multiply(a, b) { //eslint-disable-line
-  let myProd = a * b;
-  let strTwo = `The product of ${a} and ${b} is ${myProd}.`;
+  let myProd = (a * b);
+  let strTwo = (`The product of ${a} and ${b} is ${myProd}.`);
 
   return [myProd, strTwo];
 }
@@ -54,11 +54,17 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sumAndMultiply(a, b, c) { //eslint-disable-line
+  let sumTwo = sum(sum(a, b)[0], c)[0];
+  let prodTwo = multiply(multiply(a, b)[0], c)[0];
 
+  let strThree = (`${a} and ${b} and ${c} sum to ${sumTwo}.`);
+  let strFour = (`The product of ${a} and ${b} and ${c} is ${prodTwo}.`);
+
+  return [sumTwo, prodTwo, strThree, strFour];
 }
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
+testSumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
